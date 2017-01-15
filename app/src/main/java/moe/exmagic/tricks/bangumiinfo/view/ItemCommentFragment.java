2b6f6144132cba1c140fcc2a3ad66294d4d49f81
liu <chaomi1998@qq.com>
2017-01-15 15:18:09 +0800

    搞定（大概）?

--- app/src/main/java/moe/exmagic/tricks/bangumiinfo/view/ItemCommentFragment.java
+++ app/src/main/java/moe/exmagic/tricks/bangumiinfo/view/ItemCommentFragment.java
@@ -18,17 +18,11 @@ import moe.exmagic.tricks.bangumiinfo.utils.DataType;
  */
 
 public class ItemCommentFragment extends Fragment {
-    public Bitmap commentUserHeader;
-    public String commentDateTime;
-    public String commentText;
-    public String commentScore;
+    private DataType.CommentItem oneCommentItem;
 
     public static ItemCommentFragment newInstance(DataType.CommentItem item){
         ItemCommentFragment fragment = new ItemCommentFragment();
-        fragment.commentUserHeader = item.User.UserHeader;
-        fragment.commentText = item.Comment;
-        fragment.commentScore = String.valueOf(item.Score);
-        fragment.commentDateTime = String.valueOf(item.SubmitDatetime);
+        fragment.setItem(item);
         return fragment;
     }
 
@@ -36,10 +30,18 @@ public class ItemCommentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container
                            , Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.item_comment, container, false);
-        ((ImageView) v.findViewById(R.id.comment_imageViewHeader)).setImageBitmap(this.commentUserHeader);
-        ((TextView) v.findViewById(R.id.comment_textViewComments)).setText(this.commentText);
-        ((TextView) v.findViewById(R.id.comment_textViewDatetime)).setText(this.commentDateTime);
-        ((TextView) v.findViewById(R.id.textViewScore)).setText(this.commentScore);
+        ((ImageView) v.findViewById(R.id.comment_imageViewHeader))
+                .setImageBitmap(oneCommentItem.User.UserHeader);
+        ((TextView) v.findViewById(R.id.comment_textViewComments))
+                .setText(oneCommentItem.Comment);
+        ((TextView) v.findViewById(R.id.comment_textViewDatetime))
+                .setText(String.valueOf(oneCommentItem.SubmitDatetime));
+        ((TextView) v.findViewById(R.id.textViewScore))
+                .setText(String.valueOf(oneCommentItem.Score));
         return v;
+    }
+
+    public void setItem(DataType.CommentItem item){
+        oneCommentItem = item;
     }
 }
